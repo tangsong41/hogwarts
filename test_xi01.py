b35765cdae539f3931f3bdbf0d7639a896ae405b
tangsong <369223985@qq.com>
2020-12-09 22:00:26 +0800

    first commit

---
+++ test_xi01.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+@author:tangsong
+@file: test_xi01.py
+@time: 2020/12/09
+"""
+import pytest
+def setup_function():
+    print("setup_function：每个用例开始前都会执行")
+
+def teardown_function():
+    print("teardown_function：每个用例结束后都会执行")
+
+def test_one():
+    print("正在执行----test_one")
+    x = "this"
+    assert 'h' in x
+
+def test_two():
+    print("正在执行----test_two")
+    x = "hello"
+    assert hasattr(x, 'check')
+
+def test_three():
+    print("正在执行----test_three")
+    a = "hello"
+    b = "hello world"
+    assert a in b
+
+if __name__ == "__main__":
+    pytest.main(["-s", "test_fixt.py"])
+
+
+
+
+
+
+
+
+
+
+
+
