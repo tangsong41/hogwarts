d00091d5ea42bd57526788c4aab5f7f47ca7ae24
unknown <369223985@qq.com>
2021-01-12 09:51:03 +0800

    定位问题-行情

--- test_UI_frame01/page/main.py
+++ test_UI_frame01/page/main.py
@@ -14,7 +14,7 @@ class Main(BasePage):
     def goto_market(self):
         self.find_and_click(By.XPATH, '//*[@resource-id="com.xueqiu.android:id/post_status"]')
         print("post_status")
-        self.find_and_click(By.XPATH, "//*[@resource-id='com.xueqiu.android:id/tab_icon']//*[@text='行情']")
+        self.find_and_click(By.XPATH, "//*[@text='行情']")  #//*[@resource-id='com.xueqiu.android:id/tab']
         print("行情")
         return Market(self.driver)
 
